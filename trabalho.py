7a63aee448c6b8d182e11b9967a56cdc36cd0ce8
Pedro Alves Cardoso Erthal <pedro.erthal@escola.pr.gov.br>
2026-04-28 14:54:10 -0300

    1 Commit - 2 funções definidas

---
+++ trabalho.py
@@ -0,0 +1,8 @@
+def definir_nota(avaliacao):
+    nota = float(input(f"Qual foi sua nota na(o){avaliacao} "))
+    return nota
+
+def calcular_media (p1, p2, t):
+    media = (p1 * 0.35) + (p2 * 0.35) + (t * 0.3)
+    return media
+
